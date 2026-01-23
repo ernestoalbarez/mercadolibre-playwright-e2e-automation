05c9d22ef77250543766a77ea5682037a1ce0fd3
Ernesto <54443164+ernestoalbarez@users.noreply.github.com>
2026-01-23 16:38:32 -0300

    Automation: Search Filters - Price Range (#12)
    * documentation: upadte example test
    
    * fix: stop the fire; remove flaky firefox
    
    * documentation: search filter test, methods and locators
    
    * test(search): add filter by shipping

--- tests/e2e/search.spec.ts
+++ tests/e2e/search.spec.ts
@@ -20,4 +20,29 @@ test.describe('MercadoLibre - Search Filters', () => {
     await searchResultsPage.applyPriceFilter(min, max);
     await searchResultsPage.validatePriceRangeFiltering(min, max);
   });
+
+  test('should search xiaomi and filter by free shipping', async ({
+    homePage,
+    searchResultsPage,
+  }) => {
+    await homePage.searchFor('xiaomi');
+    await searchResultsPage.expectResultsToBeVisible();
+    await searchResultsPage.checkFreeShipping();
+    await searchResultsPage.expectFreeShippingIsChecked();
+  });
+
+  test('should search xiaomi, filter by price range and by free shipping', async ({
+    homePage,
+    searchResultsPage,
+  }) => {
+    const min = '300000';
+    const max = '800000';
+
+    await homePage.searchFor('xiaomi');
+    await searchResultsPage.expectResultsToBeVisible();
+    await searchResultsPage.applyPriceFilter(min, max);
+    await searchResultsPage.checkFreeShipping();
+    await searchResultsPage.expectFreeShippingIsChecked();
+    await searchResultsPage.validatePriceRangeFiltering(min, max);
+  });
 });
