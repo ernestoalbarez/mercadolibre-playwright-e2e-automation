ed607b912c914ad6f10d886c44fe0395d8d7ad42
Ernesto Albarez <ernestoalbrz@gmail.com>
2026-02-02 16:55:54 -0300

    update POM

--- fixtures/testFixtures.ts
+++ fixtures/testFixtures.ts
@@ -1,10 +1,14 @@
 import { test as base } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 import { SearchResultsPage } from '../pages/SearchResultsPage';
+import { PdpPage } from '../pages/PdpPage';
+import { CartPage } from '../pages/CartPage';
 
 type PagesFixture = {
   homePage: HomePage;
   searchResultsPage: SearchResultsPage;
+  pdpPage: PdpPage;
+  cartPage: CartPage;
 };
 
 export const test = base.extend<PagesFixture>({
@@ -13,10 +17,14 @@ export const test = base.extend<PagesFixture>({
     await homePage.open();
     await use(homePage);
   },
-  //searchResultsPage: async ({ page, homePage }, use) => {
   searchResultsPage: async ({ page }, use) => {
-    // void homePage;
     await use(new SearchResultsPage(page));
+  },
+  pdpPage: async ({ page }, use) => {
+    await use(new PdpPage(page));
+  },
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page));
   },
 });
 
